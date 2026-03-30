489cae9fab4f4299e77928087bc03b9049b96e83
Viktoria Hetman <56441437+hetman07@users.noreply.github.com>
2026-03-30 20:09:43 +0200

    Update 24_hw_task_2.py

--- 24_HW_recursia/24_hw_task_2.py
+++ 24_HW_recursia/24_hw_task_2.py
@@ -1,1 +1,21 @@
+# Сумма вложенных чисел
 
+# Напишите рекурсивную функцию, которая суммирует все числа во вложенных списках.
+
+# Данные:
+
+nested_numbers = [1, [2, 3], [4, [5, 6]], 7]
+
+# Пример вывода:
+
+# 28
+def sum_innerlist(nested_numbers: list):
+    total = 0 
+    for item in nested_numbers:
+        if isinstance(item, list):
+            total += sum_innerlist(item)
+        elif isinstance(item, int):
+            total += item
+    return total
+
+print(sum_innerlist(nested_numbers))
