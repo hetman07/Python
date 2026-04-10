463447e3fa3ced7ef9abe3aa749a76a0635e047d
Viktoria Hetman <56441437+hetman07@users.noreply.github.com>
2026-04-10 13:30:31 +0200

    Refactor sum_innerlist and add tail recursion version
    Refactor sum_innerlist to use recursion with an index and add sum_innerlist_tail for tail recursion.

--- 24_HW_recursia/24_hw_task_2.py
+++ 24_HW_recursia/24_hw_task_2.py
@@ -9,13 +9,33 @@ nested_numbers = [1, [2, 3], [4, [5, 6]], 7]
 # Пример вывода:
 
 # 28
-def sum_innerlist(nested_numbers: list):
-    total = 0 
-    for item in nested_numbers:
-        if isinstance(item, list):
-            total += sum_innerlist(item)
-        elif isinstance(item, int):
-            total += item
-    return total
+def sum_innerlist(nested_numbers: list, ind = 0):
+    if ind == len(nested_numbers):
+        return 0
+    
+    item = nested_numbers[ind]
+    
+    if isinstance(item, list):
+        return sum_innerlist(item) + sum_innerlist(nested_numbers, ind + 1)
+
+    return item + sum_innerlist(nested_numbers, ind + 1)
 
 print(sum_innerlist(nested_numbers))
+
+# с помощью хвостовой рекурсии решение задачи
+def sum_innerlist_tail(nested_numbers: list, ind = 0, acc = 0):
+    
+    if ind == len(nested_numbers):
+        return acc
+    
+    item = nested_numbers[ind]
+    
+    if isinstance(item, list):
+        acc = sum_innerlist_tail(item, 0, acc)
+    else:
+        acc += item
+        
+    return sum_innerlist_tail(nested_numbers, ind + 1, acc)
+
+print(sum_innerlist_tail(nested_numbers))
+
