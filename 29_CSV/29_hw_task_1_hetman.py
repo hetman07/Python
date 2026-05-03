7be0f4ee8e89f5e6acc290dcead9f0d7f1b46832
Viktoria Hetman <56441437+hetman07@users.noreply.github.com>
2026-05-03 18:41:47 +0200

    Implement CSV processing for grades and statistics
    The program reads grades from grades.csv and creates three separate files for Science, Math, and Physics, each containing names and grades. It also generates a grades-info.csv file with statistical data for each subject.

---
+++ 29_CSV/29_hw_task_1_hetman.py
@@ -0,0 +1,59 @@
+# На лекции при выполнении практического задания 2 
+# был получен файл grades.csv. Напишите программу, которая:
+
+# Читает этот файл.
+
+# Создает на его основе три новых файла: grades-Science.csv, 
+# grades-Math.csv, grades-Physics.csv. 
+# В каждом из этих файлов только два столбца - 
+# имя и количество баллов по тому предмету, 
+# который указан в названии файла.
+
+# Создает четвертый файл grades-info.csv. 
+# В этом файле три строки (по названиям предметов) и 
+# столбцы со статистическими характеристиками оценок: 
+#     среднее арифметическое, 
+#     минимум, 
+#     максимум, 
+#     медиана, 
+#     стандартное отклонение.
+    
+import csv
+import statistics
+
+SUBJECT = ['Science', 'Math','Physics']
+BASE_PATH = "29_CSV_statistic"
+
+buckets = {s: [] for s in SUBJECT}
+
+def count_statistic_data(grades):
+    return {
+    "avg": round(statistics.mean(grades),2),
+    "min": min(grades),
+    "max": max(grades),
+    "median": statistics.median(grades),
+    "stdev": round(statistics.stdev(grades),2)
+    }
+
+with open(f'{BASE_PATH}/grades.csv', encoding='utf-8-sig') as inp_file:
+        reader = csv.DictReader(inp_file)
+        for item in reader:
+            if item['subject'] in buckets:
+                buckets[item['subject']].append((item['name'], int(item['grade'])))
+stats_rows = []
+                
+for subject, rows in buckets.items():
+    with open(f"{BASE_PATH}/grades-{subject}.csv", "w", encoding="utf-8", newline="") as f:
+        writer = csv.writer(f) 
+        writer.writerow(["name", "grade"])
+        writer.writerows(rows)
+
+    grades = [grade for _, grade in rows]
+    stats_rows. append({"subject": subject, **count_statistic_data(grades)})
+
+fields = ["subject", "avg", "min", "max", "median", "stdev"]   
+
+with open(f"{BASE_PATH}/grades-info.csv", "w", encoding="utf-8", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=fields)
+    writer.writeheader()
+    writer.writerows(stats_rows)    
