f60140897f743b5e4bba44840cdf65d51da41aed
Viktoria Hetman <56441437+hetman07@users.noreply.github.com>
2026-05-14 20:56:22 +0200

    Create User class with user count functionality
    Implement User class with total_users counter and get_total method.

---
+++ 34_HW_Decorator/34_hw_task_1.py
@@ -0,0 +1,28 @@
+# Счётчик экземпляров
+
+# Создайте класс User, представляющий пользователя.
+# При создании должны указываться логин (username) и пароль (password).
+# У класса должно быть поле total_users,
+# хранящее общее количество созданных пользователей.
+# При каждом создании нового объекта User, 
+# счётчик должен увеличиваться.
+# Добавьте метод get_total(), возвращающий количество пользователей.
+# Проверьте, что счётчик работает.
+class User:
+    total_users = 0
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        User.total_users += 1
+
+    @classmethod
+    def get_total(cls):
+        return f"Total users: {cls.total_users}"
+
+
+user1 = User("Vita", "qwer12")
+user2 = User("Vita", "qwer12")
+user3 = User("Vita", "qwer12")
+user1 = User("Vita", "qwer12")
+print(User.get_total())
